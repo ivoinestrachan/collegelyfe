e7531cf9a2729e59b0f152e8c95e7b5a85db5d1e
Ivoine <ivoinestrachan242@gmail.com>
2023-06-01 21:43:52 -0400

    updated styling of fix some changes for school email validation

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -3,15 +3,15 @@ import React, { useState } from "react";
 const Signin = () => {
   const [email, setEmail] = useState("");
   const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   const validateEmail = () => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@(?!gmail\.com)(?:[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\.edu)$/;
+    const emailRegex =
+      /^[a-zA-Z0-9._%+-]+@(?!gmail\.com)(?:[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\.edu)$/;
     setIsValidEmail(emailRegex.test(email));
+    setIsButtonDisabled(!emailRegex.test(email));
   };
-  
-  
-  
-  
+
   const handleKeyDown = (e: { key: string }) => {
     if (e.key === "Enter") {
       validateEmail();
@@ -27,10 +27,21 @@ const Signin = () => {
           onChange={(e) => setEmail(e.target.value)}
           onBlur={validateEmail}
           onKeyDown={handleKeyDown}
+          className="border-black border rounded-sm pl-2 pr-2"
         />
         {!isValidEmail && (
-          <p className="text-red-600 font-semibold">Please enter valid school email.</p>
+          <p className="text-red-500 font-semibold">Invalid school email.</p>
         )}
+      </div>
+      <div className="text-center justify-center mt-2">
+        <button
+          className={`text-center ${
+            isButtonDisabled ? "bg-gray-400" : "bg-blue-500 text-white rounded-sm"
+          } rounded-sm py-1 pl-2 pr-2`}
+          disabled={isButtonDisabled}
+        >
+          Send Magic Link
+        </button>
       </div>
     </div>
   );
