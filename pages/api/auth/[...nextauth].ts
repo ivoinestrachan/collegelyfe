bfd25ad037ac1742ba87ac7402a8bd5a0ff93b6d
Ivoine <ivoinestrachan242@gmail.com>
2023-06-07 13:02:17 -0400

    updated code

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-import { PrismaClient } from '@prisma/client';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 const prisma = new PrismaClient();
 
@@ -9,17 +9,18 @@ export const authOptions: NextAuthOptions = {
   providers: [
     EmailProvider({
       server: {
-        host: process.env.EMAIL_SERVER_HOST || "",
+        host: process.env.EMAIL_SERVER || "",
+        port: parseInt(process.env.EMAIL_SERVER_PORT || "587"),
         auth: {
-          user: process.env.EMAIL_SERVER_USER || "", 
+          user: process.env.EMAIL_SERVER_USER || "",
           pass: process.env.EMAIL_SERVER_PASSWORD || "",
         },
       },
-      from: process.env.EMAIL_FROM || "", 
+      from: process.env.EMAIL_FROM || "",
     }),
   ],
   adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET || "", 
+  secret: process.env.SECRET || "",
   callbacks: {
     async jwt({ token }) {
       token.userRole = "admin";
